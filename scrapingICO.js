661af72d6f2c9b97fc504a35efc4782e379212c0
softcomet <professinalcertifiedsenior.dev@gmail.com>
2018-02-06 08:30:59 +0100

    Get marketvertical values

--- scrapingICO.js
+++ scrapingICO.js
@@ -5,18 +5,58 @@ const puppeteer = require('puppeteer');
   const page = await browser.newPage();
   await page.goto('https://icodrops.com/category/active-ico/');
 
-  // Get the "viewport" of the page, as reported by the page.
-  const allResultsSelector = '.tabs__content.active .category-desk.justify-content-center .a_ico a#n_color';
+  // Extract the product urls of active-ico category
+  const allResultsSelector = '.tabs__content.active .category-desk.justify-content-center .a_ico a#n_color[href]';
   await page.waitForSelector(allResultsSelector);
 
-  // Extract the results from the page.
-  const links = await page.evaluate(allResultsSelector => {
-    const anchors = Array.from(document.querySelectorAll(allResultsSelector));
-    return anchors.map(anchor => {
-      return `${anchor.href}`;
-    });
-  }, allResultsSelector);
-  console.log(links.join('\n'));
+  const links = await page.evaluate(
+    () => Array.from(document.body.querySelectorAll('.tabs__content.active .category-desk.justify-content-center .a_ico a#n_color[href]'), ({ href }) => href)
+  );
+
+  console.log(links)
+
+  for (link in links) {
+    await page.goto(links[link]);
+
+    // Get all coin names.
+    const coinnameSelector = '.white-desk.ico-desk .ico-main-info h3';
+    await page.waitForSelector(coinnameSelector);
+
+    const coinname = await page.evaluate(
+        () => document.querySelector('.white-desk.ico-desk .ico-main-info h3').innerText
+    );
+
+    // Get all Maket Verticals
+    const mvSelector = '.white-desk.ico-desk .ico-main-info .ico-category-name';
+    await page.waitForSelector(mvSelector);
+
+    const marketvertical = await page.evaluate(
+        () => document.querySelector('.white-desk.ico-desk .ico-main-info .ico-category-name').childNodes[0].nodeValue
+    );
+
+    // return {
+    //     'title': ti
+    // }
+    console.log(marketvertical)
+  }
+  
+//   Extract the detailed titles of active-ico category
+  
+//   await page.goto(links);
+//   const detailpageSelector = '.ico-main-info h3';
+//   await page.waitForSelector(detailpageSelector);
+  
+//   const titles = await page.evaluate(detailpageSelector => {
+//     const anchors = Array.from(document.querySelectorAll(detailpageSelector));
+//     return anchors.map(anchor => {     
+//       return `${anchor.text}`;
+//     });
+//   }, detailpageSelector);
+
+//   await page.evaluate( (obj) => {
+//     return obj.getAttribute('data-src');
+//   }, imgurlEle);
+
 
   await browser.close();
 
