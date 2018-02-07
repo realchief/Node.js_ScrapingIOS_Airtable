b95faa6acb75e28189c1d9b269b14c870e85c9d7
softcomet <professinalcertifiedsenior.dev@gmail.com>
2018-02-07 02:57:42 +0100

    Completing scraping data

--- scrapingICO.js
+++ scrapingICO.js
@@ -7,204 +7,197 @@ const puppeteer = require('puppeteer');
     let result = {};    
     let category_names = ['active-ico', 'active-ico', 'ended-ico']
     let category_urls = [];
-    category_names.forEach(function (category_name, i) {
-        category_url = domain_url + 'category/' + category_name
-        console.log('%d: %s', i, category_name);
-        console.log('%d: %s', i, category_url);
-    });
+    for(i = 0; i < 3; i ++){
+        category_urls[i] = domain_url + 'category/' + category_names[i];        
+        // await page.goto('https://icodrops.com/category/active-ico/');
+        await page.goto(category_urls[i]);
 
-    await page.goto('https://icodrops.com/category/active-ico/');
+        result[i] = []
+        //  Extract the product urls of active-ico category
+        const allResultsSelector = '.tabs__content.active .category-desk.justify-content-center .a_ico a#n_color[href]';
+        await page.waitForSelector(allResultsSelector);
 
-    // result['Active Ico'] = []
+        const links = await page.evaluate(
+            () => Array.from(document.body.querySelectorAll('.tabs__content.active .category-desk.justify-content-center .a_ico a#n_color[href]'), ({ href }) => href)
+        );
+        console.log(links)
+        for (link in links) {
+            let item = {};
+            item['coinname'] = coinname;
+            item['marketvertical'] = marketvertical;
+            item['description'] = description;
+            item['icon'] = icon;
+            item['presale_start_date'] = presale_start_date;
+            item['presale_end_date'] = presale_end_date;
+            item['country'] = country;
+            item['symbol'] = symbol;
+            item['target_raise'] = target_raise;
+            item['capital_raised'] = capital_raised;
+            item['tm_count'] = tm_count;
+            item['tokenprice'] = tokenprice;
+            item['twitter'] = twitter;
+            item['telegram'] = telegram;
 
+            await page.goto(links[link]);
     
-    // Extract the product urls of active-ico category
-    const allResultsSelector = '.tabs__content.active .category-desk.justify-content-center .a_ico a#n_color[href]';
-    await page.waitForSelector(allResultsSelector);
-
-    const links = await page.evaluate(
-        () => Array.from(document.body.querySelectorAll('.tabs__content.active .category-desk.justify-content-center .a_ico a#n_color[href]'), ({ href }) => href)
-    );
-
-    console.log(links)
-
-    for (link in links) {
-        item = {};
-        item['project'] = 'copytrack';
+            // Get all urls
+            url = links[link];
     
-        await page.goto(links[link]);
-
-        // Get all urls
-        url = links[link];
-
-        // Get all coin names.
-        // const coinnameSelector = '.white-desk.ico-desk .ico-main-info h3';
-        // await page.waitForSelector(coinnameSelector);
-
-        const coinname = await page.evaluate(
-            () => document.querySelector('.white-desk.ico-desk .ico-main-info h3').innerText
-        );
-
-        // Get all Maket Verticals
+            // Get all coin names.           
+            const coinname = await page.evaluate(
+                () => document.querySelector('.white-desk.ico-desk .ico-main-info h3').innerText
+            );
     
-        const marketvertical = await page.evaluate(
-            () => document.querySelector('.white-desk.ico-desk .ico-main-info .ico-category-name').childNodes[0].nodeValue.slice(1,-1)
-        );
-
-        // Get all descriptions
-        
-        const description = await page.evaluate(
-            () => document.querySelector('.white-desk.ico-desk .ico-main-info .ico-category-name').childNodes[1].innerText
-        );
-
-        // Get all icons
-
-        const icon = await page.evaluate(
-            () => document.querySelector('.white-desk.ico-desk .ico-row .ico-icon img').src
-        );
-
-        // Get all start dates of Token Sale    
-        
-        const presale_start_date = await page.evaluate(
-            () => {
-                if (document.querySelector('.row.list .col-12.title-h4 h4'))
-                    if (document.querySelector('.row.list .col-12.title-h4 h4').innerText.split(':')[1])
-                        return document.querySelector('.row.list .col-12.title-h4 h4').innerText.split(':')[1].split('–')[0]
-                    else  return ' ' 
-                else return ' '
-            }
-
-        );
-
-        const presale_end_date = await page.evaluate(
-            () => {
-                if (document.querySelector('.row.list .col-12.title-h4 h4'))
-                    if (document.querySelector('.row.list .col-12.title-h4 h4').innerText.split(':')[1])
-                        return document.querySelector('.row.list .col-12.title-h4 h4').innerText.split(':')[1].split('–')[1]
-                    else  return ' ' 
-                else return ' '
-            }
-
-        );
-
-        // Get all countries
-
-        const country = await page.evaluate(
-            () => {
-                if (document.querySelector('.col-12.info-analysis-list'))
-                    if (document.querySelector('.col-12.info-analysis-list').childNodes)
-                        if (document.querySelector('.col-12.info-analysis-list').childNodes[3])
-                            if (document.querySelector('.col-12.info-analysis-list').childNodes[3].innerText.includes('Team from'))
-                                return document.querySelector('.col-12.info-analysis-list').childNodes[3].innerText.split(':')[1]
+            // Get all Maket Verticals        
+            const marketvertical = await page.evaluate(
+                () => document.querySelector('.white-desk.ico-desk .ico-main-info .ico-category-name').childNodes[0].nodeValue.slice(1,-1)
+            );
+    
+            // Get all descriptions
+            const description = await page.evaluate(
+                () => document.querySelector('.white-desk.ico-desk .ico-main-info .ico-category-name').childNodes[1].innerText
+            );
+    
+            // Get all icons
+            const icon = await page.evaluate(
+                () => document.querySelector('.white-desk.ico-desk .ico-row .ico-icon img').src
+            );
+    
+            // Get all start dates of Token Sale    
+            const presale_start_date = await page.evaluate(
+                () => {
+                    if (document.querySelector('.row.list .col-12.title-h4 h4'))
+                        if (document.querySelector('.row.list .col-12.title-h4 h4').innerText.split(':')[1])
+                            return document.querySelector('.row.list .col-12.title-h4 h4').innerText.split(':')[1].split('–')[0]
+                        else  return ' ' 
+                    else return ' '
+                }
+    
+            );
+    
+            const presale_end_date = await page.evaluate(
+                () => {
+                    if (document.querySelector('.row.list .col-12.title-h4 h4'))
+                        if (document.querySelector('.row.list .col-12.title-h4 h4').innerText.split(':')[1])
+                            return document.querySelector('.row.list .col-12.title-h4 h4').innerText.split(':')[1].split('–')[1]
+                        else  return ' ' 
+                    else return ' '
+                }
+    
+            );
+    
+            // Get all countries
+            const country = await page.evaluate(
+                () => {
+                    if (document.querySelector('.col-12.info-analysis-list'))
+                        if (document.querySelector('.col-12.info-analysis-list').childNodes)
+                            if (document.querySelector('.col-12.info-analysis-list').childNodes[3])
+                                if (document.querySelector('.col-12.info-analysis-list').childNodes[3].innerText.includes('Team from'))
+                                    return document.querySelector('.col-12.info-analysis-list').childNodes[3].innerText.split(':')[1]
+                                else return ' '
                             else return ' '
-                        else return ' '
-                    else  return ' ' 
-                else return ' '
-            }
-        );
-
-        // Get all symbols
-
-        const symbol = await page.evaluate(
-            () => {
-                if (document.querySelector('.white-desk.ico-desk .row.list .col-12.col-md-6'))
-                    if (document.querySelector('.white-desk.ico-desk .row.list .col-12.col-md-6').childNodes)
-                        if (document.querySelector('.white-desk.ico-desk .row.list .col-12.col-md-6').childNodes[1])
-                            if (document.querySelector('.white-desk.ico-desk .row.list .col-12.col-md-6').childNodes[1].innerText.includes('Ticker'))
-                                return document.querySelector('.white-desk.ico-desk .row.list .col-12.col-md-6').childNodes[1].innerText.split(':')[1]
+                        else  return ' ' 
+                    else return ' '
+                }
+            );
+    
+            // Get all symbols
+            const symbol = await page.evaluate(
+                () => {
+                    if (document.querySelector('.white-desk.ico-desk .row.list .col-12.col-md-6'))
+                        if (document.querySelector('.white-desk.ico-desk .row.list .col-12.col-md-6').childNodes)
+                            if (document.querySelector('.white-desk.ico-desk .row.list .col-12.col-md-6').childNodes[1])
+                                if (document.querySelector('.white-desk.ico-desk .row.list .col-12.col-md-6').childNodes[1].innerText.includes('Ticker'))
+                                    return document.querySelector('.white-desk.ico-desk .row.list .col-12.col-md-6').childNodes[1].innerText.split(':')[1]
+                                else return ' '
                             else return ' '
-                        else return ' '
-                    else  return ' ' 
-                else return ' '
-            }
-        );
-
-        // Get all Target Raise
-
-        const target_raise = await page.evaluate(
-            () => {
-                if (document.querySelector('.white-desk.ico-desk .row.list .col-12.col-md-6'))
-                    if (document.querySelector('.white-desk.ico-desk .row.list .col-12.col-md-6').childNodes)
-                        if (document.querySelector('.white-desk.ico-desk .row.list .col-12.col-md-6').childNodes[7])
-                            if (document.querySelector('.white-desk.ico-desk .row.list .col-12.col-md-6').childNodes[7].innerText.includes('Fundraising Goal'))
-                                return document.querySelector('.white-desk.ico-desk .row.list .col-12.col-md-6').childNodes[7].innerText.split(':')[1]
+                        else  return ' ' 
+                    else return ' '
+                }
+            );
+    
+            // Get all Target Raise
+            const target_raise = await page.evaluate(
+                () => {
+                    if (document.querySelector('.white-desk.ico-desk .row.list .col-12.col-md-6'))
+                        if (document.querySelector('.white-desk.ico-desk .row.list .col-12.col-md-6').childNodes)
+                            if (document.querySelector('.white-desk.ico-desk .row.list .col-12.col-md-6').childNodes[7])
+                                if (document.querySelector('.white-desk.ico-desk .row.list .col-12.col-md-6').childNodes[7].innerText.includes('Fundraising Goal'))
+                                    return document.querySelector('.white-desk.ico-desk .row.list .col-12.col-md-6').childNodes[7].innerText.split(':')[1]
+                                else return ' '
                             else return ' '
-                        else return ' '
-                    else  return ' ' 
-                else return ' '
-            }
-        );
-
-        // Get all Capital Raised
-
-        const capital_raised = await page.evaluate(
-            () => {
-                if (document.querySelector('.white-desk.ico-desk .row.list .col-12.col-md-6'))
-                    if (document.querySelector('.white-desk.ico-desk .row.list .col-12.col-md-6').childNodes)
-                        if (document.querySelector('.white-desk.ico-desk .row.list .col-12.col-md-6').childNodes[9])
-                            if (document.querySelector('.white-desk.ico-desk .row.list .col-12.col-md-6').childNodes[9].innerText.includes('Sold on pre-sale'))
-                                return document.querySelector('.white-desk.ico-desk .row.list .col-12.col-md-6').childNodes[9].innerText.split(':')[1]
+                        else  return ' ' 
+                    else return ' '
+                }
+            );
+    
+            // Get all Capital Raised
+            const capital_raised = await page.evaluate(
+                () => {
+                    if (document.querySelector('.white-desk.ico-desk .row.list .col-12.col-md-6'))
+                        if (document.querySelector('.white-desk.ico-desk .row.list .col-12.col-md-6').childNodes)
+                            if (document.querySelector('.white-desk.ico-desk .row.list .col-12.col-md-6').childNodes[9])
+                                if (document.querySelector('.white-desk.ico-desk .row.list .col-12.col-md-6').childNodes[9].innerText.includes('Sold on pre-sale'))
+                                    return document.querySelector('.white-desk.ico-desk .row.list .col-12.col-md-6').childNodes[9].innerText.split(':')[1]
+                                else return ' '
                             else return ' '
-                        else return ' '
-                    else  return ' ' 
-                else return ' '
-            }
-        );
-
-        // Telegram Membership Count
-
-        const tm_count = await page.evaluate(
-            () => {
-                if (document.querySelector('.white-desk.ico-desk .row.list .col-12.info-analysis-list'))
-                    if (document.querySelector('.white-desk.ico-desk .row.list .col-12.info-analysis-list').childNodes)
-                        if (document.querySelector('.white-desk.ico-desk .row.list .col-12.info-analysis-list').childNodes[1])
-                            if (document.querySelector('.white-desk.ico-desk .row.list .col-12.info-analysis-list').childNodes[1].innerText.includes('Number of Team Members'))
-                                return document.querySelector('.white-desk.ico-desk .row.list .col-12.info-analysis-list').childNodes[1].innerText.split(':')[1]
+                        else  return ' ' 
+                    else return ' '
+                }
+            );
+    
+            // Telegram Membership Count
+            const tm_count = await page.evaluate(
+                () => {
+                    if (document.querySelector('.white-desk.ico-desk .row.list .col-12.info-analysis-list'))
+                        if (document.querySelector('.white-desk.ico-desk .row.list .col-12.info-analysis-list').childNodes)
+                            if (document.querySelector('.white-desk.ico-desk .row.list .col-12.info-analysis-list').childNodes[1])
+                                if (document.querySelector('.white-desk.ico-desk .row.list .col-12.info-analysis-list').childNodes[1].innerText.includes('Number of Team Members'))
+                                    return document.querySelector('.white-desk.ico-desk .row.list .col-12.info-analysis-list').childNodes[1].innerText.split(':')[1]
+                                else return ' '
                             else return ' '
-                        else return ' '
-                    else  return ' ' 
-                else return ' '
-            }
-        );
-
-
-        // Token Price
-        const tokenprice = await page.evaluate(
-            () => {
-                if (document.querySelector('.white-desk.ico-desk .row.list .col-12.col-md-6'))
-                    if (document.querySelector('.white-desk.ico-desk .row.list .col-12.col-md-6').childNodes)
-                        if (document.querySelector('.white-desk.ico-desk .row.list .col-12.col-md-6').childNodes[5])
-                            if (document.querySelector('.white-desk.ico-desk .row.list .col-12.col-md-6').childNodes[5].innerText.includes('Token Price'))
-                                return document.querySelector('.white-desk.ico-desk .row.list .col-12.col-md-6').childNodes[5].innerText.split(':')[1]
+                        else  return ' ' 
+                    else return ' '
+                }
+            );
+    
+    
+            // Token Price
+            const tokenprice = await page.evaluate(
+                () => {
+                    if (document.querySelector('.white-desk.ico-desk .row.list .col-12.col-md-6'))
+                        if (document.querySelector('.white-desk.ico-desk .row.list .col-12.col-md-6').childNodes)
+                            if (document.querySelector('.white-desk.ico-desk .row.list .col-12.col-md-6').childNodes[5])
+                                if (document.querySelector('.white-desk.ico-desk .row.list .col-12.col-md-6').childNodes[5].innerText.includes('Token Price'))
+                                    return document.querySelector('.white-desk.ico-desk .row.list .col-12.col-md-6').childNodes[5].innerText.split(':')[1]
+                                else return ' '
                             else return ' '
-                        else return ' '
-                    else  return ' ' 
-                else return ' '
-            }
-        );
+                        else  return ' ' 
+                    else return ' '
+                }
+            );
+    
+            // Twitter 
+            const twitter = await page.evaluate(
+                () => {
+                    if (document.querySelector('.twitter a').href)
+                        return document.querySelector('.twitter a').href
+                    else return ' '
+                }
+            );
+            
+            // telegram 
+            const telegram = await page.evaluate(
+                () => {
+                    if (document.querySelector('.telega a').href)
+                        return document.querySelector('.telega a').href
+                    else return ' '
+                }
+            );
 
-        // Twitter 
-        const twitter = await page.evaluate(
-            () => {
-                if (document.querySelector('.twitter a').href)
-                    return document.querySelector('.twitter a').href
-                else return ' '
-            }
-        );
-        
-        // telegram 
-        const telegram = await page.evaluate(
-            () => {
-                if (document.querySelector('.telega a').href)
-                    return document.querySelector('.telega a').href
-                else return ' '
-            }
-        );
-       
-        // console.log(telegram);    
-
-     
-        // result['active ico'].push(item);
-    } 
-  await browser.close();
+            // console.log(telegram);   
+            result[i].push(item);
+        }
+    }    
+    await browser.close();
 })();
